87435dd5b3b50774236a9814066b48e454dae67d
HendricksK <kurvinhendricks@gmail.com>
2020-06-07 19:50:58 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,66 @@
+// using SendGrid's Go Library
+// https://github.com/sendgrid/sendgrid-go
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"net/http"
+	"io"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+	"github.com/joho/godotenv"
+)
+
+func setupRoutes() {
+	fmt.Println("set up routes called")
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Println("/ called")
+		response := sendEmail()
+		io.WriteString(w, response)
+		fmt.Println(response)
+    })
+}
+
+func sendEmail() string {
+
+	from := mail.NewEmail("Kurvin Development", "kurvindev@gmail.com")
+	subject := "Sending with SendGrid is Fun"
+	to := mail.NewEmail("Kurvin", "kurvinhendricks@gmail.com")
+	plainTextContent := "and easy to do anywhere, even with Go"
+	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	// client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	client := sendgrid.NewSendClient(sendgridapikey)
+	response, err := client.Send(message)
+	if err != nil {
+		log.Println(err)
+
+	} else {
+		fmt.Println(response.StatusCode)
+		fmt.Println(response.Body)
+		fmt.Println(response.Headers)
+	}
+
+	return response.Body
+	
+}
+
+var sendgridapikey string 
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		fmt.Println("Error loading .env file, cannot get much done")
+	}
+
+	sendgridapikey = os.Getenv("SEND_GRID_API")
+
+	fmt.Println("main")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
+	fmt.Println("end main")
+}
